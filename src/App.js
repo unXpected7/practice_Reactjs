d934e2bac71ee771bf1d737a9e7c1110fe5a00f9
unXpected7 <ilyasyusuf002@gmail.com>
2022-02-03 21:58:38 +0700

    deploy succes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br>
+          deploy succesfully
+          </br>
         </p>
         <a
           className="App-link"
